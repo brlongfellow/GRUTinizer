f91f4fc7b718c1473a4304179bc4a3a9d0563285
Brenden Longfellow <longfell@nscl.msu.edu>
2018-03-26 14:44:45 -0400

    Updated histos to add energy and time correctly for s800 caesar coincidence

--- histos/BetterCaesarSourceSort.cxx
+++ histos/BetterCaesarSourceSort.cxx
@@ -15,6 +15,7 @@
 #include "TObject.h"
 #include "TCaesar.h"
 #include "TS800.h"
+#include "TS800Hit.h"
 
 #include "TChannel.h"
 #include "GValue.h"
@@ -26,12 +27,31 @@ int HandleUngated(TRuntimeObjects& obj) {
   TCaesar  *caesar  = obj.GetDetector<TCaesar>();
   TCaesar  *caesar_ab = new TCaesar();//will be used to do addback correction 
                                       //only for the hits inside the time cut
+  TS800    *s800    = obj.GetDetector<TS800>();
+
+  std::string histname;
+  std::string dirname;
+
+  dirname = "Ungated";
+
+  if(s800){
+    histname = "S800_E_Up";
+    obj.FillHistogram(dirname,histname,4101,-100,4000,s800->GetScint().GetEUp());
+    histname = "S800_E_Down";
+    obj.FillHistogram(dirname,histname,4101,-100,4000,s800->GetScint().GetEDown());
+    if (s800->GetME1Size()){
+      histname = "S800_Time_Up";
+      obj.FillHistogram(dirname,histname,2000,-10,10,s800->GetMTof().fE1Up[0]);
+ //   histname = "S800_Time_Down";
+ //   obj.FillHistogram(dirname,histname,2000,-10,10,s800->GetMTof().fE1Down[0]);
+    }
+  }
 
   if(!caesar)
     return false;
 
-  std::string histname;
-  std::string dirname;
+  //histname = "Fera_error_code";
+  //obj.FillHistogram(dirname,histname,5,0,5,caesar->GetFeraError());
 
   //const int SINGLES_ENERGY_THRESHOLD = 150;
   const int AB_ENERGY_THRESHOLD = 0;
@@ -51,8 +71,20 @@ int HandleUngated(TRuntimeObjects& obj) {
       if(hit.IsOverflow())
         continue;
 
+      if(!hit.IsValid())
+        continue;
+
       double energy = hit.GetEnergy();
       double charge = hit.GetCharge(); 
+      double time   = hit.GetTime();
+     
+      if(s800){
+        double corr_time = time - ((s800->GetTrigger().GetS800Source())*(0.1/0.25));
+        dirname = "Ungated";
+        histname = "Caesar Energy vs Caesar Time Relative to LaBr";
+        obj.FillHistogram(dirname,histname,2001,-2000,2000,corr_time,
+                                           2149,-100,8192,energy);
+      }
 
       caesar_ab->InsertHit(hit);
         //if (energy > SINGLES_ENERGY_THRESHOLD){              
@@ -62,18 +94,25 @@ int HandleUngated(TRuntimeObjects& obj) {
         dirname = "Ungated";
         histname = "energy";
         obj.FillHistogram(dirname,histname,
-                          1024,0,8192,energy);
-
+                          1125,-100,8192,energy);
         histname = "charge";
         obj.FillHistogram(dirname,histname,
-                          1024,0,8192,charge);
+                          1125,-100,8192,charge);
+        histname = "time";
+        obj.FillHistogram(dirname,histname,
+                          1001,-2000,2000,time);
          
         histname = "detnum_vs_energy";
         obj.FillHistogram(dirname,histname,200,0,200,hit.GetAbsoluteDetectorNumber(),
-                                           1024,0,8192,energy);
+                                           1125,-100,8192,energy);
         histname = "detnum_vs_charge";
         obj.FillHistogram(dirname,histname,200,0,200,hit.GetAbsoluteDetectorNumber(),
-                                           1024,0,8192,charge);
+                                           1125,-100,8192,charge);
+        histname = "detnum_vs_time";
+        obj.FillHistogram(dirname,histname,200,0,200,hit.GetAbsoluteDetectorNumber(),
+                                           1001,-2000,2000,time);
+
+      
 
   }//for loop over singles hits
 
@@ -94,32 +133,32 @@ int HandleUngated(TRuntimeObjects& obj) {
                         1024,0,8192,energy_ab);
       histname = "detnum_vs_energy_addback";
       obj.FillHistogram(dirname,histname,200,0,200,hit.GetAbsoluteDetectorNumber(),
-                                         1024,0,8192,energy_ab);
+                                         1125,-100,8192,energy_ab);
               
       if (hit.GetNumHitsContained() == 1 && !hit.is_garbage_addback){
         histname = "energy_addback_n0";
         obj.FillHistogram(dirname,histname,
-                          1024,0,8192,energy_ab);
+                          1125,-100,8192,energy_ab);
         energies_addback_n0.push_back(energy_ab);
 
       }
       else if (hit.GetNumHitsContained() == 2 && !hit.is_garbage_addback){
         histname = "energy_addback_n1";
         obj.FillHistogram(dirname,histname,
-                          1024,0,8192,energy_ab);
+                          1125,-100,8192,energy_ab);
         energies_addback_n1.push_back(energy_ab);
 
       }
       else if (hit.GetNumHitsContained() == 3 && !hit.is_garbage_addback){
         histname = "energy_addback_n2";
         obj.FillHistogram(dirname,histname,
-                          1024,0,8192,energy_ab);
+                          1125,-100,8192,energy_ab);
         energies_addback_n2.push_back(energy_ab);
       }
       else if(hit.is_garbage_addback){
         histname = "energy_addback_ng";
         obj.FillHistogram(dirname,histname,
-                          1024,0,8192,energy_ab);
+                          1125,-100,8192,energy_ab);
         energies_addback_ng.push_back(energy_ab);
 
       }
