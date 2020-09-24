623dffd04bfe491a473f551db2313076fefff437
Brenden Longfellow <longfell@nscl.msu.edu>
2020-09-24 18:13:46 -0400

    Commit changes before cloning onto LLNL machine

---
+++ include/GVersion.h
@@ -0,0 +1,12 @@
+#ifndef GRUT_GVERSION
+#define GRUT_GVERSION
+
+#define GRUT_RELEASE "1.02.00"
+#define GRUT_RELEASE_TIME "2016-07-18 11:07:15 -0400"
+#define GRUT_RELEASE_NAME "Chipper Chartreuse Chipmunk"
+
+#define GRUT_GIT_COMMIT "v1.02.00-174-g0556cb3"
+#define GRUT_GIT_BRANCH "root6_newcaesar"
+#define GRUT_GIT_COMMIT_TIME "2018-06-29 18:41:21 -0400"
+
+#endif /* GRUT_GVERSION */
